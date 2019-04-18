ed8da83a35e2b1931017ad419c44fb7e39ac2361
David Howard <david.howard1100@gmail.com>
2019-04-18 13:18:07 -0400

    Update Dispatcher.java

--- src/main/java/com/kennesaw/edu/os/dispatcher/Dispatcher.java
+++ src/main/java/com/kennesaw/edu/os/dispatcher/Dispatcher.java
@@ -1,32 +1,32 @@
+package com.kennesaw.edu.os.dispatcher;
+
+import com.kennesaw.edu.os.cpu.CPU;
+import com.kennesaw.edu.os.memory.PCB;
+import com.kennesaw.edu.os.memory.Memory;
+import com.kennesaw.edu.os.Driver;
+
 import java.util.*;
 
 public class Dispatcher implements Runnable {
    private CPU[] cpus; 
-   private PCB pcb;
-   private String Status;
-   private Disk disk;
+   private Memory memory;
    
-   
-   Dispatcher (CPU [] cpus, PCB pcb, String Status, Disk disk) {
+   public Dispatcher (CPU [] cpus, Memory memory) {
       this.cpus = cpus;
-      this.pcb = pcb;
-      this.Status = Status;
-      this.disk = disk;
+      this.memory = memory;
    }
    
    public void run() {//dispatcher sets the cpu process and then loads in the pcb through context switching. 
     for (CPU cpu : this.cpus) {
-		if ((pcb.getcpuID() > 0) || (!pcb.Status.RUNNING)) {
-				//pcb next;
-				//next = Scheduler.getReadyQueue().remove(); may use for later.
-				if ((pcb.getcpuID() > 0) || (!pcb.Status.RUNNING)) {
-				//Disk cache = cpu.getCache();//Is Cache located in the memory or the scheduler?
-				//for (int r = 0; r < next.getTotalSize() && r < cache.getCapacity(); r++ ) {
-					//cache.write(r, Disk.read(next, r));
-				Driver.CPU[r].load(pcb);
+		if (cpu.getPCB() == null)  {
+				cpu.getCPUStatus();
+            
+				//if ((cpu.getCpuID() > 0) || (!cpu.CPUStatus.WAITING)) {
+				//Disk cache = cpu.get
+            synchronized (cpu) {
+            cpu.notify();
             }
-         }
-   }
-  }//end method
-  
+       }
+    }
+   }//end method
 }//end class
